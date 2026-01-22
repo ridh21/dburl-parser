3ee2ea04e050124f4b6f052d6b3478009e3e3c85
ridh21 <ridhampatel2k4@gmail.com>
2026-01-22 19:38:42 +0530

    feat: added page visitors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { DM_Sans } from "next/font/google";
+import Script from "next/script";
 import { Toaster } from "@/components/ui/sonner";
 import "./globals.css";
 
@@ -22,6 +23,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="dark">
+      <head>
+        <Script
+          defer
+          src="https://cloud.umami.is/script.js"
+          data-website-id="e74504b6-4f88-4ba1-8c5c-16265cc3df32"
+          strategy="afterInteractive"
+        />
+      </head>
       <body
         className={`${dmSans.variable} font-sans antialiased min-h-screen bg-background`}
       >
